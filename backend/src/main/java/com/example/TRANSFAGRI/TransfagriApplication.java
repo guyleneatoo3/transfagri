2927e51922326066d0f4975010386c881cbd0ad0
guyleneatoo3 <guylenatoo90@gmail.com>
2025-08-11 19:49:51 +0200

    commit du frontend

---
+++ backend/src/main/java/com/example/TRANSFAGRI/TransfagriApplication.java
@@ -0,0 +1,13 @@
+package com.example.TRANSFAGRI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransfagriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransfagriApplication.class, args);
+	}
+
+}
